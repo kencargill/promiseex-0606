0f8f4e28385c97e46890e0f4ec91189db051e53d
Ken Cargill <ken.0.cargill@gmail.com>
2022-06-06 12:05:00 -0400

    all questions attempted, confusion everywhere

--- script.js
+++ script.js
@@ -3,22 +3,33 @@
 const apiPromise = fetch("https://grandcircusco.github.io/demo-apis/donuts.json")
  .then(res => res.json())
     //(1)
- .then((jsonData) => console.log(jsonData))
- .then(res => res.json())
+//  .then((jsonData) => console.log(jsonData))
     //(2)
- .then((jsonData) => console.log(jsonData))
-  
-//   const displayData = async () => {
-//     const data = await apiPromise;
-//     //(2)
-//     console.log(data.count);
-//   };
-  
-//   displayData();
+//  .then((jsonData) => console.log(jsonData.count))
 
-    //(3)
-  const countPromise = apiPromise.then((data) => {
-    //   return data.count
-    });
+    //(3) - incorrect?
+//   const countPromise = apiPromise.then((jsonData) => console.log(jsonData.count));
+//  console.log(countPromise);
 
-  console.log(countPromise);
+    //(4) - incorrect?
+// const top4Promise = apiPromise.then((jsonData) => console.log(jsonData.results[0,1,2,3]))
+
+    //(5) - returning undefined for .forEach ?
+//  const namesPromise = apiPromise.then((jsonData) => {
+//     jsonData.forEach((post) => {
+//       return post.results.name
+//     });
+//   });
+
+//   console.log(namesPromise)
+
+    //(6) - doesn't work because (5) doesn't work
+//  const capsNamePromise = namesPromise.toUpperCase
+//  console.log(capsNamePromise)
+
+    //(7) - incorrect ?
+let badPromise = fetch("https://grandcircusco.github.io/demo-apis/wrong.json").then(res => res.json());
+
+badPromise.prototype.catch((error) => {
+    console.error(error);
+  })
