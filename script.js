d550edb41f0f664bfda820d44d14b727aac002bf
Ken Cargill <ken.0.cargill@gmail.com>
2022-06-06 21:40:39 -0400

    grade response and mostly corrected code

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
 // page 12 from https://docs.google.com/document/d/1TlzyLUz3c43-HAqoMZw30xr1830Qqhi10zlAW_srDfA/preview
+// when chaining promises with .then () the return value from .then() will be transferred to the next .then() in the chain
+// if the current .then() in the chain doesnt return a value, the next .then() value will be undefined
 
-const apiPromise = fetch("https://grandcircusco.github.io/demo-apis/donuts.json")
- .then(res => res.json())
+const apiPromise = fetch('https://grandcircusco.github.io/demo-apis/donuts.json')
+    .then((response) => response.json()); //gets the data from the response object, the json data
+
     //(1)
-//  .then((jsonData) => console.log(jsonData))
+apiPromise.then((data) => console.log('step 1:', data));
+
     //(2)
-//  .then((jsonData) => console.log(jsonData.count))
+apiPromise.then((donuts) => console.log('step 2:', donuts.count));
 
-    //(3) - incorrect?
-//   const countPromise = apiPromise.then((jsonData) => console.log(jsonData.count));
-//  console.log(countPromise);
+    //(3)
+const countPromise = apiPromise.then((donuts) => donuts.count); // same as curly brackets and return donuts.count in the arrow function
+countPromise.then((count) => console.log('step 3:', count));
 
-    //(4) - incorrect?
-// const top4Promise = apiPromise.then((jsonData) => console.log(jsonData.results[0,1,2,3]))
+    //(4)
+const top4Promise = apiPromise.then((donuts) => {
+    const top4Donuts = donuts.results.slice(0,4);
+    return top4Donuts;
+});
+top4Promise.then((first4) => console.log('step 4:', first4))
 
-    //(5) - returning undefined for .forEach ?
-//  const namesPromise = apiPromise.then((jsonData) => {
-//     jsonData.forEach((post) => {
-//       return post.results.name
-//     });
+    //(5)
+const namesPromise = apiPromise.then((donuts) => donuts.results.map((donut) => donut.name));
+namesPromise.then(names => console.log('step 5:',names));
+
+    //(6) still needs work - look for jalens code
+const capsNamePromise = namesPromise.then((donutNames) => donutNames.map((name) => name.toUpperCase()));
+console.log('step 6:',capsNamePromise);
+
+    //(7) need jalen's code
+// let badPromise = fetch(
+//     "https://grandcircusco.github.io/demo-apis/wrong.json"
+// ).then(response => response.json());
+
+// badPromise.catch((err) => {
+//     console.error('failed',err);
 //   });
 
-//   console.log(namesPromise)
-
-    //(6) - doesn't work because (5) doesn't work
-//  const capsNamePromise = namesPromise.toUpperCase
-//  console.log(capsNamePromise)
-
-    //(7) - incorrect ?
-let badPromise = fetch("https://grandcircusco.github.io/demo-apis/wrong.json").then(res => res.json());
-
-badPromise.prototype.catch((error) => {
-    console.error(error);
-  })
+  //(8) need jalen's code
